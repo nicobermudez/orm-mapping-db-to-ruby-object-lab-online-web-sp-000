986b0809f17aa548f86e7c177dccc259f64eb14c
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 18:23:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,7 @@ class Student
     DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
-  def self..first_student_in_grade_10
+  def self.first_student_in_grade_10
   end
 
   def self.all_students_in_grade_X(x)
