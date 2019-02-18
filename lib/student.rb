3cec19da2224f464a72e08bc0094276320f8b631
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 18:21:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
 
   def self.all_students_in_grade_9
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 9 
+      SELECT * FROM students WHERE grade = 9
     SQL
     DB[:conn].execute(sql, grade).map {|row| self.new_from_db(row)}
   end
