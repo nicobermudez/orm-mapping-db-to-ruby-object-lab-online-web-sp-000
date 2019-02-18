8efa25f8dc25b8bfd827bbd8494b01bcec463a3e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 18:23:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,6 +75,9 @@ class Student
     DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
+  def self..first_student_in_grade_10
+  end
+
   def self.all_students_in_grade_X(x)
     sql = <<-SQL
       SELECT * FROM students WHERE grade = ?
