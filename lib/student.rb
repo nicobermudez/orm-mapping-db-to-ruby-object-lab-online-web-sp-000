4712b4879f1b2066b1b9aa12e7152f6ec77b4756
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-18 18:23:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,6 +76,10 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
   end
 
   def self.all_students_in_grade_X(x)
